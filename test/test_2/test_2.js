34ba9b01e76753a4516502a0f73ecb1424849642
handy-o <dyhan9420@gmail.com>
2022-08-15 21:55:45 +0900

    복사/공유하기 기능 추가

--- test/test_2/test_2.js
+++ test/test_2/test_2.js
@@ -136,6 +136,47 @@ $(document).ready(function() {
   }
   /* ./ 하트모션 */
   
+  
+  /* 공유하기 */
+  
+   // 카카오톡
+    window.kakaoShare = function() {
+      Kakao.init('e9d4a287cba5e05d44b6d22c73b72a89');  // 사용할 앱의 JavaScript 키
+      Kakao.Link.createDefaultButton({
+        container: '.kakao', // 카카오공유버튼ID
+        objectType: 'feed',
+        content: {
+          title: "개발새발", // 보여질 제목
+          description: "개발새발 블로그입니다", // 보여질 설명
+          imageUrl: "https://recruit.kakaobank.com", // 콘텐츠 URL
+          link: {
+            mobileWebUrl: "https://recruit.kakaobank.com",
+            webUrl: "https://recruit.kakaobank.com"
+          }
+      }
+      });
+    }
+   
+    
+  
+  // facebook
+  var link =  document.location.href;
+  window.shareFacebook = function() {
+    var sendUrl = link; // 전달할 URL
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
+  }
+  
+  // 링크
+  window.copyUrl = function(){
+    var dummy   = document.createElement("input");  // 일시적생성
+    document.body.appendChild(dummy);
+    dummy.value = link;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy); //삭제
+    $('.modal').addClass('on');
+    setTimeout(() => {$('.modal').removeClass('on')}, 3000);
+  }
    
   
   });
